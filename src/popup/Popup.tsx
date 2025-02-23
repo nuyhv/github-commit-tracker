f32b10ba06c3bc8c6da0abc6fcee0d37e158ce9c
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-02-23 17:57:49 +0900

    :sparkles: 프로젝트 초기 구성

---
+++ src/popup/Popup.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Popup = () => {
+  return (
+    <div>
+      <h1>GitHub Commit Tracker</h1>
+      <p>UI 들어갈 공간임</p>
+    </div>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<Popup />);
