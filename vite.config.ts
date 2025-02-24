9c319e4462834443d932a0847873f75d0fe871cb
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-02-24 19:18:38 +0900

    :wrench: Vite 빌드 설정 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        popup: path.resolve(__dirname, "src/popup/popup.html"),
+        background: path.resolve(__dirname, "src/background.ts"),
+      },
+      output: {
+        entryFileNames: "[name].js",
+      },
+    },
+  },
 });
